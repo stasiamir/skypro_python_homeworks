82f686fcb2644d773bb7d8e602928a7662b19457
stasiamir <maniacrain@gmail.com>
2024-06-09 16:26:16 +0300

    Update lesson_2_task_2.py

--- lesson2/lesson_2_task_2.py
+++ lesson2/lesson_2_task_2.py
@@ -1,7 +1,8 @@
 def is_year_leap(year):
     if year % 4 == 0:
         return True
+    if year % 100 == 0:
+        return False
     else:
         return False
 year = int(input("Введите год: "))
-print("Год ", year, is_year_leap(year))
